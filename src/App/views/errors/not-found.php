98b632d0929ae030e6ffd61753fb2ab09998e223
Behnam2000 <safaribehnam6@gmail.com>
2026-03-08 16:50:12 +0330

    Everything Else: Rendering the 404 Page

---
+++ src/App/views/errors/not-found.php
@@ -0,0 +1,12 @@
+<?php include $this->resolve("partials/_header.php"); ?>
+
+<section class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
+    <h1 class="font-black text-9xl">404</h1>
+    <p class="my-6 font-bold text-xl">
+        The page you are looking for was moved, removed renamed or might naver existed.
+        so GET LOST ! ...
+    </p>
+    <a href="/" class="bg-orange-400 text-white inline-block py-2 px-4 rounded">Go back to home page</a>
+</section>
+
+<?php include $this->resolve("partials/_footer.php"); ?>
